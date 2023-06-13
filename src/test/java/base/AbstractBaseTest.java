95ecd54b22a07e710132889d752118fc472ccb6c
victor <podill1122333@google.com>
2023-06-13 17:20:49 +0300

    update SpongeBob

--- src/test/java/base/AbstractBaseTest.java
+++ src/test/java/base/AbstractBaseTest.java
@@ -16,10 +16,10 @@ abstract public class AbstractBaseTest {
         driver.get("https://us.puma.com/us/en");
     }
 
-//    @AfterMethod
-//    public void closeWindow() {
-//        driver.quit();
-//    }
+    @AfterMethod
+    public void closeWindow() {
+        driver.quit();
+    }
 
     public WebDriver getDriver() {
         return driver;
